5c7994af1f5cb056c668bae8335eaa83aca39cbf
Issac Angulo <issac.angulo@gmail.com>
2020-07-07 18:55:37 -0700

    inputfixed.js snapshot working

--- inputfixed.js
+++ inputfixed.js
@@ -74,7 +74,7 @@ ifix.setProperty('badges',rmBadges);
 const rmDesc = `## Description\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n`;
 ifix.setProperty('description',rmDesc);
 
-const rmSnapshot = `## Snapshot\n./assets/vw.jpg\n`;
+const rmSnapshot = `## Snapshot\n![image](./assets/vw.jpg)\n`;
 ifix.setProperty('snapshot',rmSnapshot);
 
 const rmTableOfContents = [
